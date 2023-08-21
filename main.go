1ba3cc2e3b2adc7e87111ffb6fd36835964ab3d3
msayyids <muhammadsayyid525@gmail.com>
2023-08-21 12:17:03 +0700

    priview coccurency

--- main.go
+++ main.go
@@ -17,15 +17,16 @@ func soal1[T any](input []T) {
 	for _, v := range input {
 		fmt.Println(v)
 	}
-	time.Sleep(3 * time.Second)
 }
 
-func soal3[T interface{}](input []T, wg *sync.WaitGroup, c chan any) {
+func soal3[T any](input []T, wg *sync.WaitGroup, c chan any) {
 	defer wg.Done()
 
 	for _, v := range input {
 		c <- v
 	}
+
+	close(c)
 
 }
 
@@ -36,8 +37,14 @@ func main() {
 	var wg sync.WaitGroup
 	c := make(chan any)
 
-	go soal1(arr1)
-	go soal1(arr2)
+	// go soal1(arr1)
+	// go soal1(arr2)
+
+	go func() {
+		soal1(arr1)
+		soal1(arr2)
+		time.Sleep(2 * time.Second)
+	}()
 
 	wg.Add(4)
 
@@ -47,7 +54,6 @@ func main() {
 	go soal3(arr2, &wg, c)
 
 	wg.Wait()
-	close(c)
 
 	for result := range c {
 		fmt.Println(result)
