9cb20323bd927ee4e6d331e4c75536539aa467f2
msayyids <muhammadsayyid525@gmail.com>
2023-08-21 13:20:41 +0700

    revisi

--- main.go
+++ main.go
@@ -6,57 +6,38 @@ import (
 	"time"
 )
 
-func soal2[T any](input []T, wg *sync.WaitGroup) {
-	defer wg.Done()
+func letters(input []string) {
 	for _, v := range input {
 		fmt.Println(v)
 	}
 }
 
-func soal1[T any](input []T) {
+func numbers(input []int) {
 	for _, v := range input {
 		fmt.Println(v)
 	}
-}
-
-func soal3[T any](input []T, wg *sync.WaitGroup, c chan any) {
-	defer wg.Done()
-
-	for _, v := range input {
-		c <- v
-	}
-
-	close(c)
-
 }
 
 func main() {
-
-	arr1 := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
-	arr2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	letter := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+	number := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	var wg sync.WaitGroup
-	c := make(chan any)
-
-	// go soal1(arr1)
-	// go soal1(arr2)
 
 	go func() {
-		soal1(arr1)
-		soal1(arr2)
-		time.Sleep(2 * time.Second)
+		letters(letter)
+		numbers(number)
 	}()
 
-	wg.Add(4)
+	time.Sleep(2 * time.Second)
 
-	go soal2(arr1, &wg)
-	go soal2(arr2, &wg)
-	go soal3(arr1, &wg, c)
-	go soal3(arr2, &wg, c)
+	wg.Add(1)
+
+	go func() {
+		letters(letter)
+		numbers(number)
+		wg.Done()
+	}()
 
 	wg.Wait()
-
-	for result := range c {
-		fmt.Println(result)
-	}
 
 }
